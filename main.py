c5f53fe63024b37816310a25206c1b613f0af1d1
P. Dworak <82469107+wendl31@users.noreply.github.com>
2025-11-22 23:26:00 +0100

    Added more things.

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-
-
 def get_name_os():
     with open("/etc/os-release", "r") as file:
         for line in file:
@@ -42,8 +40,8 @@ def get_cpu_cores():
 def get_ram_free():
     with open("/proc/meminfo", "r") as file:
         for line in file:
-            if line.startswith("MemAvailable:"):  # lepší než MemFree
-                return line.split()[1]  # jen číslo v kB
+            if line.startswith("MemAvailable:"):  
+                return line.split()[1]  
 
 def get_ram_total():
     with open("/proc/meminfo", "r") as file:
@@ -51,17 +49,29 @@ def get_ram_total():
             if line.startswith("MemTotal:"):
                 return line.split()[1]
 
-              
 
+
+def get_uptime():  
+    with open("/proc/uptime", "r") as file:
+        for line in file:
+            cislo = line.split()
+            min_cislo = float(cislo[0]) / 60
+            min_cislo  = round(min_cislo, 2)
+            print(f"Uptime {min_cislo} mins")
+        
 def main():
     get_host_name()
+    print()
     get_name_os()
     get_timezone()
+    print()
     print("----Hardware Info----")
+    print()
     get_cpu_name()
     get_cpu_cores()
     print(f"RAM: {get_ram_total()} kB / {get_ram_free()} kB")
-   
+    print()
+    get_uptime()
 
 
 
